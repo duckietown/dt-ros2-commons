bf61f972ced47b0de4df089fd80c3d46660d971e
Akshet Patel <akshetp.ap@gmail.com>
2024-04-07 22:13:15 +0100

    DTSW-5347

--- launchers/my-subscriber.sh
+++ launchers/my-subscriber.sh
@@ -5,6 +5,9 @@ source /environment.sh
 # initialize launch file
 dt-launchfile-init
 
+# launch publisher
+ros2 run my_package my_publisher_node &
+
 # launch subscriber
 ros2 run my_package my_subscriber_node
 
